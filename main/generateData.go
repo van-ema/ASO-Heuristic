1cf409fd9251a6c851b0bbb211b3cb7da3f259a4
Emanuele Vannacci <emanuele.vannacci@gmail.com>
2018-03-02 18:49:13 +0100

    My first GoLang program :)

---
+++ main/generateData.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"math/rand"
+	"fmt"
+	"time"
+)
+
+const (
+	ORDER_N      = 300 /* #Orders */
+	MOVER_N      = 30  /* #Movers */
+	MAX_DISTANCE = 200 /* Max distance between two orders*/
+	STD          = 30  /* standard deviation for delivery times*/
+	MEAN         = 90  /* Mean for delivery times */
+)
+
+func CreateOrderMatrix(nOrder, nMover int) [][]int {
+	rand.Seed(time.Now().Unix())
+
+	m1 := make([][]int, nOrder+nMover)
+	for i := range m1 {
+		m1[i] = make([]int, nOrder)
+	}
+
+	for i := 0; i < nOrder+nMover; i++ {
+		for j := 0; j < nOrder; j++ {
+
+			if i == j {
+				m1[i][j] = 0
+			} else {
+				m1[i][j] = NextRandom(0, MAX_DISTANCE)
+			}
+		}
+	}
+
+	return m1
+}
+
+func createDeliveryTimeVector(nOrder int) []int {
+	v := make([]int, nOrder)
+	for i := 0; i < nOrder; i++ {
+		v[i] = int(rand.NormFloat64()*STD + MEAN)
+	}
+
+	return v
+}
+
+func NextRandom(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func main() {
+	fmt.Print(CreateOrderMatrix(ORDER_N, MOVER_N))
+	fmt.Println("\n")
+	fmt.Println(createDeliveryTimeVector(ORDER_N))
+}
