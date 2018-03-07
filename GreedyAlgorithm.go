d160cdf3f68d887c9902f8914970b94975cb2a1c
Emanuele Vannacci <emanuele.vannacci@gmail.com>
2018-03-07 02:38:31 +0100

    modified algorithm

--- GreedyAlgorithm.go
+++ GreedyAlgorithm.go
@@ -13,22 +13,20 @@ const (
 )
 
 type Order struct {
-	id int 		// order's id
+	id   int    // order's id
 	name string // alphanumeric name
-	x  int 		// target delivery time
-	t  int 		// delivery time
+	x    int    // target delivery time
+	t    int    // delivery time
 }
 
 type Distances [][]int
 type DeliveryTimeVector []int
-
 
 var nOrder int
 var nMover int
 
 var distances [][]int
 var deliveryTimes DeliveryTimeVector
-
 
 /* orderIndexToName[order index] = alphanumeric ID (given in CSV files) */
 var orderIndexToName map[int]string
@@ -38,69 +36,105 @@ var orderIndexToName map[int]string
  */
 var moverIndexToName map[int]string
 
-// return for each mover a list of orders
-func GreedySolver(totalCost *int) ([][]uint8, []int, []uint8, []uint8, []uint8, []uint8) {
+// n: number of orders
+// m: number of mover
+func GreedySolver(n int, m int, totalCost *int) ([][]uint8, []int, []uint8, []uint8, []uint8, []uint8) {
 
-	nRow := nMover + nOrder
-	nCol := nOrder
+	nRow := n + m
+	nCol := n
 	y := make([][]uint8, nRow)
 	for i := 0; i < nRow; i++ {
 		y[i] = make([]uint8, nCol)
 	}
 
-	x := make([]int, nOrder)
-	w := make([]uint8, nOrder)
+	x := make([]int, n)
+	w := make([]uint8, n)
 
-	z := make([]uint8, nOrder)
-	z1 := make([]uint8, nOrder)
-	z2 := make([]uint8, nOrder)
+	z := make([]uint8, n)
+	z1 := make([]uint8, n)
+	z2 := make([]uint8, n)
 
-	// alias for D - D* : contains the orders not assigned
-	orders := initOrder(deliveryTimes)
-	// keep the effective length of the list
-	// from position length to the end of the list we can find scheduled orders
-	length := orders.Len()
+	orders := initOrder(deliveryTimes, nOrder)
 
 	// keep the total cost of the solution
 	*totalCost = 0
-	// Solving for each mover
-	for mover := 0; mover < nMover; mover++ {
+	// Each partition is a list of the orders assigned to the mover
+	orderPartitions := make([]*list.List, nMover)
+	for i := 0; i < nMover; i++ {
+		orderPartitions[i] = new(list.List)
+	}
+	// list of cancelled orders
+	cancelled := new(list.List)
 
-		// Orders are never removed from the orders list but they are moved on the back
-		// We use the var length to keep the number of orders not already scheduled
-		// placed at the beginning of the list
-		*totalCost += SingleMoverSchedulingOrders(mover, y, x, z, z1, z2, orders, &length)
+	// Phase-1: find an assignment of orders to the movers
+	toSchedule := orders.Front()
+	for h := 1; h <= n; h++ {
 
+		minCost := utils.Inf
+		bestMover := -1
+		for mover := 0; mover < nMover; mover++ {
+
+			// add temporary the order to schedule to the partition of the current mover
+			orderPartitions[mover].PushFront(toSchedule.Value.(*Order))
+
+			cost := SingleMoverSchedulingOrders(mover, orderPartitions[mover])
+			if minCost < cost {
+				bestMover = mover
+			}
+
+			orderPartitions[mover].Remove(toSchedule)
+		}
+
+		toScheduleOrder := toSchedule.Value.(*Order)
+		// The schedule is feasible for bestMover
+		if bestMover != -1 {
+			orderPartitions[bestMover].PushFront(toSchedule)
+		} else {
+			cancelled.PushFront(toSchedule)
+			*totalCost += 10
+			w[toScheduleOrder.id] = 1
+		}
+
+		toSchedule = toSchedule.Next()
 	}
 
-	// The orders still in the list are all those that cannot be scheduled
-	e := orders.Front()
-	for k := 0; k < length; k++ {
-		*totalCost += 10
-		cancelled := e.Value.(*Order)
-		w[cancelled.id] = 1
-		e = e.Next()
+	// Phase-2 : Re-compute final schedule for each mover and update output structures
+	for mover := 0; mover < nMover; mover++ {
 
+		partition := orderPartitions[mover]
+		cost := SingleMoverSchedulingOrders(mover, orderPartitions[mover])
+		*totalCost += cost
+
+		e := partition.Front()
+		prev := e
+		y[nOrder+mover][e.Value.(*Order).id] = 1
+		for ; e != nil; e = e.Next() {
+			order := e.Value.(*Order)
+			x[order.id] = order.x
+			y[prev.Value.(*Order).id][e.Value.(*Order).id] = 1
+			prev.Next()
+		}
 	}
 
 	return y, x, w, z, z1, z2
 }
 
-// return a list of orders that can be scheduled by the mover
-func SingleMoverSchedulingOrders(mover int, y [][]uint8, x []int, z, z1, z2 []uint8, orders *list.List, length *int) int {
+// return the cost to schedule orders in the list
+func SingleMoverSchedulingOrders(mover int, orders *list.List) int {
 
 	cost := 0
 	var lastOrder = new(Order)
 	lastOrder.id = nOrder + mover
 
-	for i := 0; i < *length; i++ {
+	length := orders.Len()
+	for i := 0; i < length; i++ {
 
 		var minOrderElem *list.Element // order that minimize the cost
 		minCost := utils.Inf           // keep the cost of the favourable order
 		newDeliveryTime := 0           // keep the delivery time of the last scheduled order
 
 		current := orders.Front()
-		for j := 0; j < *length; j++ {
+		for j := 0; j < length; j++ {
 			order := current.Value.(*Order)
 
 			newCost, nextDeliveryTime := computeCost(lastOrder.id, newDeliveryTime, order)
@@ -115,28 +149,18 @@ func SingleMoverSchedulingOrders(mover int, y [][]uint8, x []int, z, z1, z2 []ui
 
 		// schedule not feasible, try with the next mover
 		if minCost == utils.Inf {
-			break
+			return minCost
 		}
 
 		// schedule is feasible
 		cost += minCost
 		minOrder := minOrderElem.Value.(*Order)
+		minOrder.x = newDeliveryTime
 
-		// Update results
-		if minCost == 1 {
-			z[minOrder.id] = 1
-		} else if minCost == 2 {
-			z1[minOrder.id] = 1
-		} else if minCost == 3 {
-			z2[minOrder.id] = 1
-		}
-
-		x[minOrder.id] = newDeliveryTime
-		y[lastOrder.id][minOrder.id] = 1
 		// Remove order from list of orders to schedule
 		orders.MoveToBack(minOrderElem)
 		lastOrder = minOrder
-		*length--
+		length--
 		i--
 	}
 
@@ -146,9 +170,8 @@ func SingleMoverSchedulingOrders(mover int, y [][]uint8, x []int, z, z1, z2 []ui
 
 // orders allocation and initialization with delivery times
 // We use linked list because we must frequently remove assigned orders
-func initOrder(deliveryTimes []int) *list.List {
+func initOrder(deliveryTimes []int, n int) *list.List {
 
-	n := len(deliveryTimes)
 	orderList := list.New()
 
 	for i := 0; i < n; i++ {
@@ -202,31 +225,6 @@ func computeCost(lastOrderId int, lastDeliveryTime int, nextOrder *Order) (int, 
 
 }
 
-func printResults(results [][]*Order) {
-	for k := range results {
-		/*if k >= 0 {
-			fmt.Printf("%s%d : ", "Mover-", k)
-		} else {
-			fmt.Printf("cancelled:")
-		} */
-		if k == len(results)-1 { /* */
-			fmt.Printf("cancelled:")
-		} else {
-			fmt.Printf("%s%d : ", "Mover-", k)
-		}
-
-		printArray(results[k])
-
-		fmt.Print("\n")
-	}
-}
-
-func printArray(p []*Order) {
-	for _, v := range p {
-		fmt.Printf("[id:%d,x:%d,t:%d]", v.id, v.x, v.t)
-	}
-}
-
 // return for each mover a list of orders that he can schedule
 // For each order it find the mover that minimize the cost
 func BaseSolver(totalCost *int) ([][]uint8, []int, []uint8, []uint8, []uint8, []uint8) {
@@ -246,7 +244,7 @@ func BaseSolver(totalCost *int) ([][]uint8, []int, []uint8, []uint8, []uint8, []
 	z2 := make([]uint8, nOrder)
 
 	// alias for D - D* : contains the orders not assigned
-	orders := initOrder(deliveryTimes)
+	orders := initOrder(deliveryTimes, nOrder)
 	// keep the effective length of the list
 	// from position length to the end of the list we can find scheduled orders
 	length := orders.Len()
@@ -323,49 +321,9 @@ func schedule(order *Order, lastOrders []*Order, y [][]uint8, x []int, w []uint8
 	return bestMover, minCost
 }
 
-func main() {
-	nOrder = ORDER_N
-	nMover = MOVER_N
-
-	/*
-	distances = utils.CreateOrderMatrix(nOrder, nMover)
-	deliveryTimes = utils.CreateDeliveryTimeVector(nOrder) */
-	distances, deliveryTimes = getInput()
-
-	utils.PrintDistanceMatrix(distances, nOrder)
-	fmt.Print("Algorithm 1:\n")
-	start := time.Now()
-	var cost int
-	y, x, w, z, z1, z2 := GreedySolver(&cost)
-	elapsed := time.Since(start)
-	//printResults(res)
-
-	utils.PrintAssigmentMatrix(y, nOrder)
-	fmt.Println(x)
-	fmt.Println(w)
-	fmt.Println(z, z1, z2)
-	fmt.Printf("Solver took %s\n", elapsed)
-	fmt.Printf("Total cost: %d\n", cost)
-
-	fmt.Print("\n\nAlgorithm 2:\n")
-	start = time.Now()
-	y, x, w, z, z1, z2 = BaseSolver(&cost)
-	elapsed = time.Since(start)
-	//printResults(res)
-
-	utils.PrintAssigmentMatrix(y, nOrder)
-	fmt.Println(x)
-	fmt.Println(w)
-	fmt.Println(z, z1, z2)
-	fmt.Printf("Solver took %s\n", elapsed)
-	fmt.Printf("Total cost: %d\n", cost)
-
-}
-
-
-func getInput() (distMat Distances,deliveryTime DeliveryTimeVector) {
+func getInput() (distMat Distances, deliveryTime DeliveryTimeVector) {
 	/* init */
-	distMat = make(Distances, ORDER_N + MOVER_N )
+	distMat = make(Distances, ORDER_N+MOVER_N)
 	deliveryTime = make(DeliveryTimeVector, ORDER_N)
 	orderIndexToName = make(map[int]string)
 	moverIndexToName = make(map[int]string)
@@ -381,19 +339,19 @@ func getInput() (distMat Distances,deliveryTime DeliveryTimeVector) {
 		orderIndexToName[orderKey.I] = orderKey.N
 		deliveryTime[orderKey.I] = deliveryTimesMap[orderKey.N]
 
-		for j,distance := range distVector {
+		for j, distance := range distVector {
 			distMat[orderKey.I][j] = distance
 		}
 
 	}
 
 	for moverKey, distVector := range moverOrderDistMat {
-		distMat[ORDER_N + moverKey.I] = make([]int, ORDER_N)
+		distMat[ORDER_N+moverKey.I] = make([]int, ORDER_N)
 
 		/* update additional mover info */
 		moverIndexToName[moverKey.I] = moverKey.N
 		for j, distance := range distVector {
-			distMat[ORDER_N + moverKey.I][j] = distance
+			distMat[ORDER_N+moverKey.I][j] = distance
 		}
 	}
 
@@ -407,4 +365,42 @@ func getInput() (distMat Distances,deliveryTime DeliveryTimeVector) {
 	} */
 
 	return distMat, deliveryTime
+}
+
+func main() {
+	nOrder = ORDER_N
+	nMover = MOVER_N
+
+	//distances = utils.CreateOrderMatrix(nOrder, nMover)
+	//deliveryTimes = utils.CreateDeliveryTimeVector(nOrder)
+
+	distances, deliveryTimes = getInput()
+	utils.PrintDistanceMatrix(distances, nOrder)
+	fmt.Print("Algorithm 1:\n")
+	start := time.Now()
+	var cost int
+	y, x, w, z, z1, z2 := GreedySolver(nOrder, nMover, &cost)
+	elapsed := time.Since(start)
+	//printResults(res)
+
+	utils.PrintAssigmentMatrix(y, nOrder)
+	fmt.Println(x)
+	fmt.Println(w)
+	fmt.Println(z, z1, z2)
+	fmt.Printf("Solver took %s\n", elapsed)
+	fmt.Printf("Total cost: %d\n", cost)
+
+	//fmt.Print("\n\nAlgorithm 2:\n")
+	//start = time.Now()
+	//y, x, w, z, z1, z2 = BaseSolver(&cost)
+	//elapsed = time.Since(start)
+	////printResults(res)
+	//
+	//utils.PrintAssigmentMatrix(y, nOrder)
+	//fmt.Println(x)
+	//fmt.Println(w)
+	//fmt.Println(z, z1, z2)
+	//fmt.Printf("Solver took %s\n", elapsed)
+	//fmt.Printf("Total cost: %d\n", cost)
+
 }
