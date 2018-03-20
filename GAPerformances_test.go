d7b277a6b3162628ce36e8548192828a9bbd2b59
trillaura <laura.trivelloni@gmail.com>
2018-03-20 11:14:57 +0100

    delete useless files & final fix bench

--- GAPerformances_test.go
+++ GAPerformances_test.go
@@ -18,22 +18,24 @@ func BenchmarkGreedySolver(b *testing.B) {
 	utils.DistanceMatrixFilename = "input/"+ utils.DistanceMatrixFilename
 
 	for N:= 1; N<39; N++ {
-		CANC_TOT := 0
-		COST_TOT := 0
-		var TIME_TOT time.Duration = 0
+
 
 		moverPolicy = MINIMIZE_ACTIVE_MOVERS
 
 		test:
 
+		CANC_TOT := 0
+		COST_TOT := 0
+		var TIME_TOT time.Duration = 0
+
 		for i := 0; i < RUNS; i++ {
 			nMover = N
 			results, elapsed := execute()
-			CANC_TOT = results.nCancelled
-			COST_TOT = results.totalCost
+			CANC_TOT += results.nCancelled
+			COST_TOT += results.totalCost
 			TIME_TOT += elapsed
 		}
-		printTimes(N, nOrder-CANC_TOT, TIME_TOT, COST_TOT, CANC_TOT)
+		printTimes(N, nOrder-CANC_TOT/RUNS, TIME_TOT/RUNS, COST_TOT/RUNS, CANC_TOT/RUNS)
 
 		if moverPolicy == MAXIMIZE_ACTIVE_MOVERS {
 			continue
@@ -65,7 +67,7 @@ func printTimes(n int, o int, t time.Duration, cost int, canc int) {
 		"RESULTS AVERAGED AMONG %d RUNS"+
 			"\n\t\t%d ORDERS ASSIGNED IN %v"+
 			"\n\t\t%d CANCELED ORDERS"+
-			"\n\t\tTOTAL COST = %d \n", RUNS, o, t/RUNS, canc, cost)
+			"\n\t\tTOTAL COST = %d \n", RUNS, o, t, canc, cost)
 
 	fmt.Printf("\n\n")
 }
